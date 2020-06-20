36982170f102e23d4beaac29b263b50a75bd8682
Kevin Brutus <kevinbrutus@pursuit.org>
2020-06-20 11:18:10 -0400

    update restaurant feed

--- tastebuds/src/components/RestaurantFeed.js
+++ tastebuds/src/components/RestaurantFeed.js
@@ -1,26 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
+import RestaurantPage from './RestaurantPage'
 
 const RestaurantFeed = ({restaurants}) => {
+    // let [choice, setChoice] = useState("")
     
-    const handleClick = () => {
-
+    const handleClick = (e) => {
+        // setChoice(e.target.children)
+        debugger
     }
-
+    
     return(
         <>
             <>
                 {restaurants.map(restaurant => {
                     return(
-                        <div className = 'restaurants' onClick = {handleClick}>
+                        <ul>
+                        <li>
+                        <button className = 'restaurants' value={restaurant.restaurant_id} onClick = {handleClick}>
                              <h3>{restaurant.restaurant_name}</h3>
                              <p>{restaurant.cuisines}</p>
                              <p>{restaurant.price_range}</p>
                              <p>{restaurant.hours}</p>
-                        </div>
+                        </button>
+                        </li>
+                        </ul>
                     )
                 })}
             </>
         </>
     )
 }
+{/* <RestaurantPage restaurant={restaurant}/> */}
+
+
 export default RestaurantFeed
