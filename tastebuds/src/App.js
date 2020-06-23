be813b20fdc5d345657716b978c3e7f2ae4a293b
Samantha Jimenez <samantha@Samanthas-MacBook-Pro.local>
2020-06-22 23:57:19 -0400

    create navbar js and css

--- tastebuds/src/App.js
+++ tastebuds/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom'
 import Welcome from '../src/components/WelcomePage'
 import RestaurantPage from './components/RestaurantPage';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
     <section>
+    <NavBar/>
     <Switch>
       <Route exact path={"/"}>
         <Welcome />
