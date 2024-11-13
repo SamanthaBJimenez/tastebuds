8b6613d3d3e4e1bf50bf04a3db4ea9bb2ca7c894
Kevin Brutus <kevinbrutus@pursuit.org>
2024-11-13 17:35:00 -0500

    add hover style to restaurant buttons

--- tastebuds/src/components/RestaurantFeed.js
+++ tastebuds/src/components/RestaurantFeed.js
@@ -1,28 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import '../css/RestaurantFeed.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../css/RestaurantFeed.css";
 
-const RestaurantFeed = ({restaurants}) => {
-    
-    const restaurantList = restaurants.map((restaurant, i) => {
-        return <Link key={i} to={`/restaurant/${restaurant.restaurant_id}`}><li className = 'restaurant_li'>
-                <button name={restaurant.restaurant_id} className = 'restaurant_button' value={restaurant.restaurant_name} >
-                    <h3 className = 'feed_name'>{restaurant.restaurant_name}</h3>
-                    <p className = 'feed_cuisine'>{restaurant.cuisines}</p>
-                    <p className = 'feed_price'>{restaurant.price_range}</p>
-                    <p className = 'feed_hours'>{restaurant.hours}</p>
-                </button>
+const RestaurantFeed = ({ restaurants }) => {
+  const restaurantList = restaurants.map((restaurant, i) => {
+    return (
+      <Link key={i} to={`/restaurant/${restaurant.restaurant_id}`}>
+        <li className="restaurant_li">
+          <button
+            name={restaurant.restaurant_id}
+            className="restaurant_button"
+            value={restaurant.restaurant_name}
+          >
+            <h3 className="feed_name">{restaurant.restaurant_name}</h3>
+            <p className="feed_cuisine">{restaurant.cuisines}</p>
+            <p className="feed_price">{restaurant.price_range}</p>
+            <p className="feed_hours">{restaurant.hours}</p>
+          </button>
         </li>
-        </Link>
-    })
+      </Link>
+    );
+  });
 
-    return(
-        <ul className = 'restaurant_ul'>
-            {restaurantList}
-            {restaurantList.length ? restaurantList : <div className = 'feedBlurbDiv'><h1 className='feed_blurbOne'>Where Are We Going To Eat?</h1><h3 className = 'feed_blurbTwo'>Type your zip code in the search bar to find restaurants near you.</h3></div>}
-        </ul>
+  return (
+    <div>
+      {restaurantList.length ? (
+        <ul className="restaurant_ul">{restaurantList}</ul>
+      ) : (
+        <div className="feedBlurbDiv">
+          <h1 className="feed_blurbOne">Where Are We Going To Eat?</h1>
+          <h3 className="feed_blurbTwo">
+            Type your zip code in the search bar to find restaurants near you.
+          </h3>
+        </div>
+      )}
+    </div>
+  );
+};
 
-    )
-}
-
-export default RestaurantFeed
+export default RestaurantFeed;
